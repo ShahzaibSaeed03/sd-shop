2a13d5e3feede2e0df84deb20346cc2221d86a4b
Umair Tariq - Idev4 <umairtariq1011@gmail.com>
2026-04-07 03:05:57 +0500

    fix language modal

--- src/app/shared/components/header/header.ts
+++ src/app/shared/components/header/header.ts
@@ -11,29 +11,38 @@ export class Header {
   @Input() menuItems: any[] = [];
   @Input() coins: number = 0;
   @Input() language: string = 'PT/BR';
-@Output() openLogin = new EventEmitter<'login' | 'points' | 'profile'>();
+  @Output() openLogin = new EventEmitter<'login' | 'points' | 'profile'>();
   openLoginModal() {
     this.openLogin.emit('points'); // 🔥 send type
   }
 
- 
-isLoggedIn = false; // change later from API
 
-openPointsModal() {
-  if (this.isLoggedIn) {
-    this.openLogin.emit('profile');
-  } else {
-    this.openLogin.emit('login');
+  isLoggedIn = false; // change later from API
+  isLanguageModalOpen = false;
+
+  openPointsModal() {
+    if (this.isLoggedIn) {
+      this.openLogin.emit('profile');
+    } else {
+      this.openLogin.emit('login');
+    }
   }
-}
 
   isMenuOpen = false;
 
-toggleMenu() {
-  this.isMenuOpen = !this.isMenuOpen;
-}
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
 
-closeMenu() {
-  this.isMenuOpen = false;
-}
+  closeMenu() {
+    this.isMenuOpen = false;
+  }
+
+  openLanguageModal() {
+    this.isLanguageModalOpen = !this.isLanguageModalOpen;
+  }
+
+  closeLanguageModal() {
+    this.isLanguageModalOpen = false;
+  }
 }
