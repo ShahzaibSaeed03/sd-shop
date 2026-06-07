e7acdcc6190308f9eb607d806ae7ec82c0dcf69c
Shahzaib Saeed <shahzaibsaeed077@gmail.com>
2026-06-07 14:25:26 +0500

    fix review bg

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  // apiUrl: 'https://api.sdshop.gg/api'
-    apiUrl: 'http://localhost:5000/api'
+  apiUrl: 'https://api.sdshop.gg/api'
+    // apiUrl: 'http://localhost:5000/api'
 
 };
